d82f5849f64de4e310d26ab3bd8df8af4732830c
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 <ts3657@gmail.com>
2021-12-03 11:24:10 +0530

    fatch data from local data in graphql #practice

---
+++ fakedb.js
@@ -0,0 +1,26 @@
+export const users = [
+  {
+    id: "1",
+    firstName: "tushar",
+    lastName: "shrivastav",
+    email: "ts3657@gmail.com",
+    password: "123456789",
+  },
+  {
+    id: "2",
+    firstName: "sushar",
+    lastName: "shrivastav",
+    email: "ss3657@gmail.com",
+    password: "123456789",
+  },
+];
+export const quotes = [
+  {
+    name: "i turn coffe into code",
+    by: "1",
+  },
+  {
+    name: "if your code work it dont touch it",
+    by: "2",
+  },
+];
