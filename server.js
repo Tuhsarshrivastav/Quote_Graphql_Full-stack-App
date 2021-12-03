4eff646052f88330ded25936901ef888d8170367
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 <ts3657@gmail.com>
2021-12-03 10:28:17 +0530

    setup graphql in nodejs

---
+++ server.js
@@ -0,0 +1,22 @@
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
+const typeDefs = gql`
+  type Query {
+    greet: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greet: () => "hello world",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+server.listen().then(({ url }) => {
+  console.log(`sever ${url}`);
+});
