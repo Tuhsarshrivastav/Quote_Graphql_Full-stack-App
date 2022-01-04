a2f2ce4c6db397a2d55f3d17cb1fe546ea2d9d0d
Tuhar Shrivastav <ts3657@gmail.com>
2022-01-04 13:49:20 +0530

    added besic graphql server in nodejs

--- server.js
+++ server.js
@@ -1,35 +1,26 @@
-import { ApolloServer } from "apollo-server";
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
-import typeDefs from "./schemaGql.js";
+import {ApolloServer,gql} from 'apollo-server'
+import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core'
 
-import mongoose from "mongoose";
-import { mongodb } from "./config.js";
 
-mongoose.connect(mongodb, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const typeDefs = gql`
+  type Query{
+      greet:String
+  }
 
-mongoose.connection.on("connected", () => {
-  console.log("connected to mongodb");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.log("error connecting", err);
-});
-
-//import models here
-import "./models/Quotes.js";
-import "./models/User.js";
-
-import resolvers from "./resolvers.js";
-
+`
+const resolvers ={
+    Query:{
+        greet:()=> "hello world"
+    }
+}
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
-});
+    typeDefs,
+    resolvers,
+    plugins:[
+        ApolloServerPluginLandingPageGraphQLPlayground
+    ]
+})
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+server.listen().then(({url})=>{
+  console.log(`server is running ${url}`)
+})
