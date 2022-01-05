edc9793215129744aeb85591170078632d559983
Tuhar Shrivastav <ts3657@gmail.com>
2022-01-05 15:23:13 +0530

    added signup feature with hashedpassword

--- resolvers.js
+++ resolvers.js
@@ -1,9 +1,11 @@
 import { quotes, users } from "./fakedb.js";
-import { randomBytes } from "crypto";
+import mongoose from "mongoose";
+const User = mongoose.model("User");
+import bcryptjs from "bcryptjs";
 export const resolvers = {
   Query: {
     users: () => users,
-    user: (_, { id }) => users.find((user) => user.id == id),
+    user: (_, { _id }) => users.find((user) => user._id == _id),
     quotes: () => quotes,
     iquote: (_, { by }) => quotes.filter((quote) => quote.by == by),
   },
@@ -11,13 +13,17 @@ export const resolvers = {
     quotes: (ur) => quotes.filter((quote) => quote.by == ur.id),
   },
   Mutation: {
-    signupUserDummy: (_, { newUser }) => {
-      const id = randomBytes(5).toString("hex");
-      users.push({
-        id,
-        ...newUser
-      })
-      return users.find((user) => user.id == id);
+    signupUser: async (_, { newUser }) => {
+      const user = await User.findOne({ email: newUser.email });
+      if (user) {
+        throw new Error("User already exists with that email");
+      }
+      const hashedPassword = await bcryptjs.hash(newUser.password, 12);
+      const newUsers = new User({
+        ...newUser,
+        password: hashedPassword,
+      });
+      return await newUsers.save();
     },
   },
 };
--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 import {ApolloServer} from 'apollo-server'
 import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core'
-import {resolvers} from './resolvers.js'
 import typeDefs from './schemaGql.js'
 import dotenv from 'dotenv'
 dotenv.config()
-import { Database } from './database.js'
+import mongoose from 'mongoose'
 
-Database()
+mongoose.connect(process.env.db,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+})
+
+mongoose.connection.on("connected",()=>{
+  console.log("connected to mongodb")
+})
+
+mongoose.connection.on("error",(err)=>{
+  console.log("error connecting",err)
+})
+//import models here
+import './models/Quotes.js'
+import './models/User.js'
+
+import {resolvers} from './resolvers.js'
+
 
 const server = new ApolloServer({
     typeDefs,
