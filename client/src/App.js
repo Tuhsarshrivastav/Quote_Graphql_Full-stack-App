8300d38372ace1dd36ad075a4ede54a1bbdd5202
Tuhar Shrivastav <ts3657@gmail.com>
2022-01-07 08:31:56 +0530

    added react-router-dom-v6

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { useRoutes } from "react-router";
+import { routes } from "./routes";
 function App() {
+  const element = useRoutes(routes);
   return (
-    <div >
-      hello
-    </div>
+    <>
+      <NavBar />
+      {element}
+    </>
   );
 }
 
